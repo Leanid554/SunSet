fa9aa4c20aaefdc4fc9f95c72bc97115ed7b65b4
BE3Y4IY <olegmat111@gmail.com>
2025-02-06 20:53:49 +0100

    Zablokoroval Bloki.Bloki откроются только если у предыдущего 100% прогресса

--- src/pages/BlockPage/BlockPage.jsx
+++ src/pages/BlockPage/BlockPage.jsx
@@ -1,4 +1,3 @@
-// src/pages/BlockPages/BlockPages.jsx
 import React from "react";
 import { useParams } from "react-router-dom";
 import Block from "../../components/Block/Block"; // Импортируем Block
@@ -20,40 +19,39 @@ const videosData = {
   2: [
     { id: 21, title: "Lekcja1", progress: 100 },
     { id: 22, title: "Lekcja2", progress: 100 },
-    { id: 23, title: "Lekcja3", progress: 100 },   //1n 1block
+    { id: 23, title: "Lekcja3", progress: 100 },
     { id: 24, title: "Lekcja4", progress: 100 },
-    { id: 25, title: "Lekcja5", progress: 50 },  //2n 2block
+    { id: 25, title: "Lekcja5", progress: 50 },
     { id: 26, title: "Lekcja6", progress: 0 },
     { id: 27, title: "Lekcja7", progress: 0 },
     { id: 28, title: "Lekcja8", progress: 0 },
     { id: 29, title: "Lekcja9", progress: 0 },
-    { id: 52, title: "Test 2", progress: 0 }, //5n айди тестов для блоков
+    { id: 52, title: "Test 2", progress: 0 },
   ],
   3: [
     { id: 31, title: "Lekcja1", progress: 100 },
     { id: 32, title: "Lekcja2", progress: 100 },
-    { id: 33, title: "Lekcja3", progress: 100 },   //1n 1block
+    { id: 33, title: "Lekcja3", progress: 100 },
     { id: 34, title: "Lekcja4", progress: 100 },
-    { id: 35, title: "Lekcja5", progress: 50 },  //2n 2block
+    { id: 35, title: "Lekcja5", progress: 50 },
     { id: 36, title: "Lekcja6", progress: 0 },
     { id: 37, title: "Lekcja7", progress: 0 },
     { id: 38, title: "Lekcja8", progress: 0 },
     { id: 39, title: "Lekcja9", progress: 0 },
-    { id: 53, title: "Test 3", progress: 0 }, //5n айди тестов для блоков
+    { id: 53, title: "Test 3", progress: 0 },
   ],
   4: [
     { id: 41, title: "Lekcja1", progress: 100 },
     { id: 42, title: "Lekcja2", progress: 100 },
-    { id: 43, title: "Lekcja3", progress: 100 },   //1n 1block
+    { id: 43, title: "Lekcja3", progress: 100 },
     { id: 44, title: "Lekcja4", progress: 100 },
-    { id: 45, title: "Lekcja5", progress: 50 },  //2n 2block
+    { id: 45, title: "Lekcja5", progress: 50 },
     { id: 46, title: "Lekcja6", progress: 0 },
     { id: 47, title: "Lekcja7", progress: 0 },
     { id: 48, title: "Lekcja8", progress: 0 },
     { id: 49, title: "Lekcja9", progress: 0 },
-    { id: 54, title: "Test 4", progress: 0 }, //5n айди тестов для блоков
+    { id: 54, title: "Test 4", progress: 0 },
   ],
-  
 };
 
 function BlockPages() {
