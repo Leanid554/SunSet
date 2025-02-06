3d9df35621bc6394a3502e7ff20eb8f7da52dffd
BE3Y4IY <olegmat111@gmail.com>
2025-02-06 17:12:34 +0100

    Design Main

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,25 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./index.scss"; 
+import "./index.scss"; // Импортируем стили для MainPage
 
-const BlockLink = ({ title, path }) => (
+const BlockLink = ({ title, path, progress }) => (
   <div className="block-link">
     <Link to={path} className="block-button">
-      {title}
+      <div className="video-content">
+        <h3>{title}</h3>
+        {progress !== undefined && (
+          <div className="progress-bar">
+            <div
+              className="progress"
+              style={{ width: `${progress}%` }}
+            ></div>
+          </div>
+        )}
+      </div>
     </Link>
   </div>
 );
 
 function MainPage() {
+  // Прогресс для блоков
+  const block1Progress = 80; // Пример прогресса для Block 1
+  const block2Progress = 40; // Пример прогресса для Block 2
+  
+
   return (
     <div className="main-page">
       <div className="block-containerMain">
-      <BlockLink title="Block 1" path="/block/1" />
-      <BlockLink title="Block 2" path="/block/2" />
-
+        <BlockLink title="Block 1" path="/block/1" progress={block1Progress} />
+        <BlockLink title="Block 2" path="/block/2" progress={block2Progress} />
       </div>
-
-      
     </div>
   );
 }
